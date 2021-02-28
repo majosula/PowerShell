9dae62d4a725ff88b60f2a6bde7b0a220e58849f
majosula <75697582+majosula@users.noreply.github.com>
2021-02-28 20:40:57 +0100

    DNS Check

---
+++ DSN_check.ps1
@@ -0,0 +1,24 @@
+﻿#DNS check
+#GET DNS IPs
+$DNS = Get-DnsClientServerAddress| where {$_.InterfaceAlias -like "*Ether*" -and $_.ServerAddresses -ne ""} | select -Property ServerAddresses
+$IPS = $DNS.ServerAddresses
+
+#write your DNS IPs into console
+Write-Host "Your DNS are:" -ForegroundColor Yellow
+$IPS
+
+#for each DNS test connection on port 53 and write output
+foreach ($IP in $IPS)
+    {
+        $testConnection = Test-NetConnection $IP -Port 53 -InformationLevel Quiet
+
+        if ($testConnection -eq "True")
+        {
+            Write-Host -ForegroundColor Green "DNS" $IP "is working fine. Connection  via port 53 is open."
+        }
+
+    else 
+        {
+            Write-Host -ForegroundColor red "DNS" $IP "is not workign properly"
+        }
+    }
